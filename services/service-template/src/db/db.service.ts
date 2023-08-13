05a3f8b54b18a6681b52fceacbe72669263cd25b
skazska <skazska@gmail.com>
2023-08-13 08:15:04 +0300

    HW-2: boilerplate

--- services/service-template/src/db/db.service.ts
+++ services/service-template/src/db/db.service.ts
@@ -4,9 +4,30 @@ import knex, { Knex } from 'knex';
 
 @Injectable()
 export class DbService {
-  public readonly knex: Knex;
+  private _knex: Knex;
+
+  private schemaName = 'service-template';
+
+  protected get q(): Knex.QueryBuilder {
+    return this._knex.withSchema(this.schemaName);
+  }
 
   constructor(private config: AppConfigService) {
-    this.knex = knex({ client: 'pg', connection: this.config.dbUrl });
+    this._knex = knex({ client: 'pg', connection: this.config.dbUrl });
+  }
+
+  public async init(): Promise<void> {
+    const schema = this._knex.schema;
+    await schema.dropSchemaIfExists(this.schemaName, true);
+    await schema.createSchemaIfNotExists(this.schemaName);
+
+    const hasTable = await schema.hasTable('service-template');
+
+    if (hasTable) return;
+
+    await schema.createTable('service-template', (qb) => {
+      qb.increments('id');
+      qb.string('name');
+    });
   }
 }
