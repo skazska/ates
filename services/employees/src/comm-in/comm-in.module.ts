b122d10d5547d3a83a040e431657d07ceb09a729
skazska <skazska@gmail.com>
2023-08-14 07:12:42 +0300

    HW-2

--- services/employees/src/comm-in/comm-in.module.ts
+++ services/employees/src/comm-in/comm-in.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { KafkaController } from './kafka.controller';
 import { HttpController } from './http.controller';
 import { EmployeesHttpController } from './employees.http.controller';
+import { EmployeeModule } from '../employee/employee.module';
 
 @Module({
   controllers: [EmployeesHttpController, HttpController, KafkaController],
+  imports: [EmployeeModule],
 })
 export class CommInModule {}
