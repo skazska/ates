9623db5e56d10fb0919e1eb3a4f8e3e982289922
skazska <skazska@gmail.com>
2023-08-14 07:49:17 +0300

    HW-2

--- services/auth/src/login/login.module.ts
+++ services/auth/src/login/login.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { LoginService } from './login.service';
 
 @Module({
+  exports: [LoginService],
   providers: [LoginService],
 })
 export class LoginModule {}
